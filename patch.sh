1773cae17ea5d9d32e58b13067be2b939577e0f9
pixelomer <37103802+pixelomer@users.noreply.github.com>
2022-12-28 17:16:10 +0300

    Symlink libfmodstudio.so.13 and libfmod.so.13

--- patch.sh
+++ patch.sh
@@ -60,5 +60,7 @@ cp -H otherlibs/fmodstudioapi20206linux/api/studio/lib/arm64/libfmodstudio.so.13
 cp -H otherlibs/fmodstudioapi20206linux/api/core/lib/arm64/libfmod.so.13.6 "${celeste_dir}/lib-arm64/libfmod.so.13.6"
 cp -H otherlibs/*.so* "${celeste_dir}/lib-arm64/"
 cd "${celeste_dir}/lib-arm64/"
+ln -s libfmodstudio.so.13.6 libfmodstudio.so.13
+ln -s libfmod.so.13.6 libfmod.so.13
 
 echo "[+] Successfully patched Celeste"
