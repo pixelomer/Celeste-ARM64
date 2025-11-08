f37d8019df5d28543719400e3475d1ea4fb51b40
pixelomer <37103802+pixelomer@users.noreply.github.com>
2025-11-08 18:33:04 +0300

    chore: build.sh: formatting, comments

--- build.sh
+++ build.sh
@@ -2,16 +2,14 @@
 
 set -e
 
-# CELESTE_FMOD2_USE_DOCKER
-# Only really useful for arm64 macOS users who want to compile libraries
-# for an arm64 Ubuntu 24.04 target (e.g. a Nintendo Switch)
 if [ "${CELESTE_FMOD2_USE_DOCKER}" == 1 ]; then
 	type docker 2>/dev/null >&2 || {
 		echo "Docker is missing, cannot use Docker"
 		exit 1
 	}
-	sudo docker build -t celeste-arm64/build docker
-	sudo docker run --rm -t --user "$(id -u):$(id -g)" -v "$(pwd)":"/work" celeste-arm64/build bash -c "cd /work; CELESTE_FMOD2_SKIP_DEPS=1 ./build.sh"
+	sudo docker build -t celeste-fmod2/build docker
+	sudo docker run --rm -t --user "$(id -u):$(id -g)" -v "$(pwd)":"/work" celeste-fmod2/build \
+		bash -c "cd /work; CELESTE_FMOD2_SKIP_DEPS=1 ./build.sh"
 	exit 0
 fi
 
